5b4d022c437c7c54a26ff44c9e588c2bfe64ca8a
Edwin <edwin@accredify.io>
2020-11-28 14:29:28 +0800

    add simple rule to pass code formatting

---
+++ classes/GumballMachine.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace classes;
+
+class GumballMachine
+{
+
+    private $gumballs;
+
+    public function getGumballs()
+    {
+        return $this->gumballs;
+    }
+
+    public function setGumballs($amount)
+    {
+        $name = "edwin";
+        $name = 123;
+        echo $name;
+
+        $this->gumballs = $amount;
+    }
+
+    public function turnWheel()
+    {
+        $this->setGumballs($this->getGumballs() - 1);
+    }
+}
